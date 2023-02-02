6be3b13aa289d7029b688540d41a6f61929f0149
Maryna <maryna.lysyak@gmail.com>
2023-02-02 12:16:51 +0300

    ДЗ первого семинара

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+System.Console.Write("Введите число  N1: ");
+int N1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите число  N2: ");
+int N2 = Convert.ToInt32(Console.ReadLine());
+
+if ( N1 > N2)
+{
+    Console.Write("N1 больше N2 ");
+}
+else if (N1 == N2)
+{
+    System.Console.Write("Числа равны ");
+}
+else
+{
+    Console.Write("N2 ,больше N1");
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+System.Console.WriteLine("Введите число N1");
+int N1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число N2");
+int N2 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число N3");
+int N3 = Convert.ToInt32(Console.ReadLine());
+
+if (N1 > N2 && N1 > N3)
+{
+    System.Console.WriteLine(N1);
+}
+
+else if (N2 > N3 && N2 > N1)
+{
+    System.Console.WriteLine(N2);
+}
+else 
+{
+    System.Console.WriteLine(N3);
+}
